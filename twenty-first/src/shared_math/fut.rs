6dd545e0395ae83f758828e1fde5e24248e7dc36
ulrik elmelund <ulrikelmelund@gmail.com>
2022-10-05 23:57:31 +0200

    hush some warnings

--- twenty-first/src/shared_math/fut.rs
+++ twenty-first/src/shared_math/fut.rs
@@ -1,15 +1,13 @@
-use generated_lib::{Array_i32_2d, Error, FutharkContext};
-
 #[cfg(test)]
 #[test]
 fn futhark_mmul() {
     let a = vec![1, 2, 3, 4];
     let b = vec![2, 3, 4, 1];
 
-    let mut ctx = FutharkContext::new().unwrap();
+    let mut ctx = generated_lib::FutharkContext::new().unwrap();
 
-    let a_arr = Array_i32_2d::from_vec(ctx, &a, &[2, 2]).unwrap();
-    let b_arr = Array_i32_2d::from_vec(ctx, &b, &[2, 2]).unwrap();
+    let a_arr = generated_lib::Array_i32_2d::from_vec(ctx, &a, &[2, 2]).unwrap();
+    let b_arr = generated_lib::Array_i32_2d::from_vec(ctx, &b, &[2, 2]).unwrap();
 
     let res_arr = ctx.matmul(a_arr, b_arr).unwrap();
 
